3d9231057c46cdcc73fb4c678d2d996dc6116be6
Alexandre <a-palo@laposte.net>
2016-10-16 20:45:08 +0200

    database sample for user

---
+++ data/users.js
@@ -0,0 +1,11 @@
+const users = {
+  1: {
+    first_name: 'Alexandre',
+    last_name: 'PALO',
+    password: '12345', // hashed
+    date_birth: '01/28/1993', // mm/dd/yyyy
+    email: 'a-palo@laposte.net'
+  },
+}
+
+module.exports = users
