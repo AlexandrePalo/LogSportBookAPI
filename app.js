15ce446daf99b11b8c0de5dfdc499dd2675a8eea
Alexandre <a-palo@laposte.net>
2016-10-16 22:22:21 +0200

    separating files

--- app.js
+++ app.js
@@ -11,88 +11,9 @@ app.use(bodyParser.json())
 // Database connection
 mongoose.connect('mongodb://admin:admin@ds059306.mlab.com:59306/logsportbookapi')
 
-// Testing schemas
-var User = require('./models/user')
-
-router.route('/users')
-
-  .post(function (req, res) {
-
-    var user = new User()
-    user.first_name = req.body.first_name
-    user.last_name = req.body.last_name
-    user.email = req.body.email
-    user.password = req.body.password
-
-    // Send the created user is better
-    // How to obtain the current id ?
-    user.save(function(err) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.json({ message: 'User created!' })
-      }
-    })
-
-  })
-
-  .get(function (req, res) {
-    User.find( function(err, users) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.json(users)
-      }
-    })
-  })
-
-router.route('/users/:user_id')
-
-  .get(function (req, res) {
-    User.findById(req.params.user_id, function(err, user) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.json(user)
-      }
-    })
-  })
-
-  .put(function (req, res) {
-    User.findById(req.params.user_id, function(err, user) {
-      if (err) {
-        res.send(err)
-      } else {
-        user.first_name = req.body.first_name
-
-        user.save(function (err) {
-          if (err) {
-            res.send(err)
-          } else {
-            res.json(user)
-          }
-        })
-      }
-    })
-  })
-
-  .delete(function (req, res) {
-    User.findById(req.params.user_id, function(err, user) {
-      if (err) {
-        res.send(err)
-      } else {
-        user.remove(function (err) {
-          if (err) {
-            res.send(err)
-          } else {
-            res.json({ message: 'deleted !'})
-          }
-        })
-      }
-    })
-  })
-
-app.use('/api', router)
+// Connecting routes
+var routerUsers = require('./routes/users')
+app.use('/api/users', routerUsers)
 
 app.listen(3000, function () {
   console.log('Listening localhost:3000')
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,84 @@
-module.exports = function(app){
+var express = require('express')
+var router = express.Router()
 
-    app.get('/users', function(req, res) {
-      res.send('List of users')
+var User = require('../models/user')
+
+router.route('/')
+
+  .post(function (req, res) {
+
+    var user = new User()
+    user.first_name = req.body.first_name
+    user.last_name = req.body.last_name
+    user.email = req.body.email
+    user.password = req.body.password
+
+    // Send the created user is better
+    // How to obtain the current id ?
+    user.save(function(err) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.json({ message: 'User created!' })
+      }
     })
-    app.get('/users/:id', function(req, res) {
-      res.send('User ' + req.params['id'])
+
+  })
+
+  .get(function (req, res) {
+    User.find( function(err, users) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.json(users)
+      }
     })
+  })
 
+router.route('/:user_id')
 
-}
+  .get(function (req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.json(user)
+      }
+    })
+  })
+
+  .put(function (req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) {
+        res.send(err)
+      } else {
+        user.first_name = req.body.first_name
+
+        user.save(function (err) {
+          if (err) {
+            res.send(err)
+          } else {
+            res.json(user)
+          }
+        })
+      }
+    })
+  })
+
+  .delete(function (req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) {
+        res.send(err)
+      } else {
+        user.remove(function (err) {
+          if (err) {
+            res.send(err)
+          } else {
+            res.json({ message: 'deleted !'})
+          }
+        })
+      }
+    })
+  })
+
+module.exports = router
