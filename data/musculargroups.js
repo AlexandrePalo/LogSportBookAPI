a99330d19f1265988673d42ac900242ceae6e772
Alexandre <a-palo@laposte.net>
2016-10-16 14:30:08 +0200

    add initial state musculargroups

---
+++ data/musculargroups.js
@@ -0,0 +1,19 @@
+const musculargroups = {
+  1: {
+    name: 'Ceinture scapulaire'
+  },
+  2: {
+    name: 'Membres supérieurs'
+  },
+  3: {
+    name: 'Membres inférieurs'
+  },
+  4: {
+    name: 'Paroi abdominale'
+  },
+  5: {
+    name: 'Paroi dorsale'
+  }
+}
+
+module.exports = musculargroups
