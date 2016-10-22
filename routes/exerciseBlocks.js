e73c3f92f277d2c1108709dba824328eabd0cc40
Alexandre <a-palo@laposte.net>
2016-10-22 10:12:03 +0200

    crud router for exerciceBlocks

--- routes/exerciseBlocks.js
+++ routes/exerciseBlocks.js
@@ -1,86 +1,18 @@
 var express = require('express')
 var router = express.Router()
 
-var ExerciseBlock = require('../models/exerciseBlock')
+var ExerciceBlock = require('../models/exerciceBlock')
 
+var actionsExerciceBlocks = require('../actions/exerciceBlocks')
+
+// CRUD
 router.route('/')
-
-  .post(function (req, res) {
-
-    var exerciseBlock = new ExerciseBlock()
-    exerciseBlock.training = req.body.training
-    exerciseBlock.index = req.body.index
-    exerciseBlock.date_begin = new Date(req.body.date_begin)
-    exerciseBlock.date_end = new Date(req.body.date_end)
-    // Send the created exerciseBlock is better
-    // How to obtain the current id ?
-    exerciseBlock.save(function(err) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.json({ message: 'Created!' })
-      }
-    })
-
-  })
-
-  .get(function (req, res) {
-    ExerciseBlock.find( function(err, exerciseBlock) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.json(exerciseBlock)
-      }
-    })
-  })
+  .post(function (req, res) { actionsExerciceBlocks.create(req.body, res) })
+  .get(function (req, res) { actionsExerciceBlocks.list(req.query, res) })
 
 router.route('/:exerciseBlock_id')
-
-  .get(function (req, res) {
-    ExerciseBlock.findById(req.params.exerciseBlock_id, function(err, exerciseBlock) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.json(exerciseBlock)
-      }
-    })
-  })
-
-  .put(function (req, res) {
-    ExerciseBlock.findById(req.params.exerciseBlock_id, function(err, exerciseBlock) {
-      if (err) {
-        res.send(err)
-      } else {
-        exerciseBlock.training = req.body.training
-        exerciseBlock.index = req.body.index
-        exerciseBlock.date_begin = new Date(req.body.date_begin)
-        exerciseBlock.date_end = new Date(req.body.date_end)
-
-        exerciseBlock.save(function (err) {
-          if (err) {
-            res.send(err)
-          } else {
-            res.json(exerciseBlock)
-          }
-        })
-      }
-    })
-  })
-
-  .delete(function (req, res) {
-    ExerciseBlock.findById(req.params.exerciseBlock_id, function(err, exerciseBlock) {
-      if (err) {
-        res.send(err)
-      } else {
-        exerciseBlock.remove(function (err) {
-          if (err) {
-            res.send(err)
-          } else {
-            res.json({ message: 'Deleted !'})
-          }
-        })
-      }
-    })
-  })
+  .get(function (req, res) { actionsExerciceBlocks.retrieve(req.params.exerciseBlock_id, res) })
+  .put(function (req, res) { actionsExerciceBlocks.update(req.params.exerciseBlock_id, req.body, res) })
+  .delete(function (req, res) { actionsExerciceBlocks.remove(req.params.exerciseBlock_id, res) })
 
 module.exports = router
