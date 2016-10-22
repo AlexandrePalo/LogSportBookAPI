8e1df4166efa4ea7b2ab91b3ff117aefd2ef04ad
Alexandre <a-palo@laposte.net>
2016-10-22 15:22:13 +0200

    new routes for muscularGroup

---
+++ actions/muscularGroups.js
@@ -0,0 +1,74 @@
+var MuscularGroup = require('../models/muscularGroup')
+var _ = require('lodash')
+
+const create = function (muscularGroup, res) {
+  var instance = new MuscularGroup({
+    name: muscularGroup.name
+  })
+  instance.save(function(err) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.json(instance)
+    }
+  })
+}
+
+const list = function (query, res) {
+  MuscularGroup.find(query, function(err, instances) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.json(instances)
+    }
+  })
+}
+
+const retrieve = function (query, res) {
+  MuscularGroup.findOne(query, function(err, instance) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.json(instance)
+    }
+  })
+}
+
+const update = function (query, data, res) {
+  MuscularGroup.findOne(query, function(err, instance) {
+    if (err) {
+      res.send(err)
+    } else {
+      for (var attr in data) {
+        if (_.includes(Object.keys(MuscularGroup.schema.paths), attr)) {
+          instance[attr] = data[attr]
+        }
+      }
+      instance.save(function(err) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.json(instance)
+        }
+      })
+    }
+  })
+}
+
+const remove = function (query, res) {
+  MuscularGroup.findOne(query, function(err, instance) {
+    if (err) {
+      res.send(err)
+    } else {
+      instance.remove(function (err) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.json(instance)
+        }
+      })
+    }
+  })
+}
+
+module.exports = { create, list, retrieve, update, remove }
